a9063f546e1ed3a03297855fbd38702a30f191df
Sam Nguyen <samxnguyen@gmail.com>
2012-03-02 10:43:23 -0800

    refactoring

--- flowtable.js
+++ flowtable.js
@@ -1,3 +1,9 @@
+// flowtable.js v1.0.0
+// Copyright (c) 2012 Sam Nguyen
+// flowtable.js is freely distributable under the MIT license
+// http://www.opensource.org/licenses/mit-license.php
+// Homepage: http://dtjm.github.com/js-flowtable
+
 (function(global){
     var calculateColumnWidths = function(data) {
         var maxWidths = [];
@@ -42,47 +48,69 @@
         return index - lineIndex;
     };
 
-    var translateIndex = function(index, args) {
+    var translateIndex = function(index, args, newLines) {
         var newIndex = 0;
         var cursorLine = getCursorLine(index, args);
         var cursorCol  = getCursorCol(index, args);
         for(var i = 0; i < cursorLine; i++) {
-            newIndex += args.outputLines[i].length + 1;
+            newIndex += newLines[i].length + 1;
         }
         return newIndex + cursorCol;
     };
 
+    var getPosition = function(index, oldLines) {
+        var lineIndex = 0;
+        var numLines = oldLines.length;
+        var lineLengths = oldLines.map(function(l){return l.length;});
+        var line, col;
+        for(var i = 0; i < numLines; i++) {
+            lineIndex += lineLengths[i] + 1;
+            if(index < lineIndex) {
+                line = i;
+                break;
+            }
+        }
+
+        lineIndex = 0;
+        for(var i = 0; i < line; i++) {
+            lineIndex += lineLengths[i] + 1;
+        }
+        return [line, index - lineIndex];
+    };
+
     // args: delimiter, selection array, text
     global.flowTable = function(args){
-        args.lines = args.text.split("\n");
+        var oldLines = args.lines = args.text.split("\n");
         var data = [];
-        args.lines.forEach(function(l){
+        oldLines.forEach(function(l){
             data.push(l.split(args.splitter));
         });
 
         var widths = calculateColumnWidths(data);
 
-        var cursorLine = getCursorLine(args.selectionEnd, args);
+        var position = getPosition(args.selectionEnd, oldLines);
+        var cursorLine = position[0];
 
         var text = "";
-        args.outputLines = [];
+        var newLines = [];
         data.forEach(function(row, i){
             // If the cursor is on the current line, don't format it
             if(cursorLine === i) {
-                args.outputLines.push(args.lines[i]);
+                newLines.push(args.lines[i]);
                 return;
             }
             var cols = [];
             row.forEach(function(col, j) {
                 cols.push(padText(col.trim(), widths[j]));
             });
-            args.outputLines.push(cols.join(args.joiner));
+            newLines.push(cols.join(args.joiner));
         });
+        args.outputLines = newLines;
 
         return {
             text: args.outputLines.join("\n"),
-            selectionStart: translateIndex(args.selectionStart, args),
-            selectionEnd: translateIndex(args.selectionEnd, args)
+            selectionStart: translateIndex(args.selectionStart, args, newLines),
+            selectionEnd: translateIndex(args.selectionEnd, args, newLines)
         };
     };
 
